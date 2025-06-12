b70f811bdbce7729af2ebfb262aa171b62d2e900
LINUXexpert.org <51408202+LINUXexpert-org@users.noreply.github.com>
2025-06-12 16:03:33 -0700

    Update rsync_magic.sh

--- rsync_magic.sh
+++ rsync_magic.sh
@@ -1,6 +1,20 @@
 #!/bin/bash
 
 # rsync_magic.sh - A smart, feature-rich rsync wrapper for safe and powerful file synchronization.
+# 
+# Copyright (C) 2025 LINUXexpert.org
+# 
+# This program is free software: you can redistribute it and/or modify it 
+# under the terms of the GNU General Public License as published by the 
+# Free Software Foundation, version 3 of the License.
+# 
+# This program is distributed in the hope that it will be useful, but 
+# WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY 
+# or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License 
+# for more details.
+# 
+# You should have received a copy of the GNU General Public License along 
+# with this program. If not, see <https://www.gnu.org/licenses/>.
 
 set -euo pipefail
 
